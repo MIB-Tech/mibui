8e98304cd658ce1dbc5ca3c76447f9099fc0975c
ABDERRAHMANE ELFANNIR <a.elfannir@akdital.ma>
2023-07-26 12:33:45 +0100

    Preview Block

--- src/pages/Components/Button/Page.tsx
+++ src/pages/Components/Button/Page.tsx
@@ -20,75 +20,73 @@ const Page = () => {
 
   return (
     <div>
-      <div className="flex items-center gap-2">
-        <div>
-          <Button
-            size={ButtonSizeEnum.Small}
-            onClick={() => setSize(ButtonSizeEnum.Small)}
-            active={size === ButtonSizeEnum.Small}
-          >
-            Small
-          </Button>
+      <PreviewBlock title='Controllers'>
+        <div className="flex items-center gap-2">
+          <div>
+            <Button
+              size={ButtonSizeEnum.Small}
+              onClick={() => setSize(ButtonSizeEnum.Small)}
+              active={size === ButtonSizeEnum.Small}
+            >
+              Small
+            </Button>
+          </div>
+          <div>
+            <Button
+              onClick={() => setSize(undefined)}
+              active={!size}
+            >
+              Default
+            </Button>
+          </div>
+          <div>
+            <Button
+              size={ButtonSizeEnum.Large}
+              onClick={() => setSize(ButtonSizeEnum.Large)}
+              active={size === ButtonSizeEnum.Large}
+            >
+              Large
+            </Button>
+          </div>
+          <div>
+            <Button
+              onClick={() => setDisabled(!disabled)}
+              active={disabled}
+            >
+              Disabled
+            </Button>
+          </div>
+          <div>
+            <Button
+              onClick={() => setActive(!active)}
+              active={active}
+            >
+              Active
+            </Button>
+          </div>
         </div>
-        <div>
-          <Button
-            onClick={() => setSize(undefined)}
-            active={!size}
-          >
-            Default
-          </Button>
-        </div>
-        <div>
-          <Button
-            size={ButtonSizeEnum.Large}
-            onClick={() => setSize(ButtonSizeEnum.Large)}
-            active={size === ButtonSizeEnum.Large}
-          >
-            Large
-          </Button>
-        </div>
-        <div>
-          <Button
-            onClick={() => setDisabled(!disabled)}
-            active={disabled}
-          >
-            Disabled
-          </Button>
-        </div>
-        <div>
-          <Button
-            onClick={() => setActive(!active)}
-            active={active}
-          >
-            Active
-          </Button>
-        </div>
-      </div>
+      </PreviewBlock>
+
 
       {(Object.values(ButtonVariant) as ButtonVariant[]).map(variant => {
 
         return (
-          <div key={variant} className="pt-10">
-            <div className="text-2xl capitalize">
-              {variant}
-            </div>
-            <div className="flex gap-2 pt-2">
-              {(Object.values(ColorVariantEnum) as ColorVariantEnum[]).map(color => (
-                <Button
-                  key={variant + color}
-                  variant={variant}
-                  color={{color: color}}
-                  className="capitalize"
-                  size={size}
-                  disabled={disabled}
-                  active={active}
-                >
-                  {/*<BellIcon className='header-icon'/>*/}
-                  {color}
-                </Button>
-              ))}
-            </div>
-          </div>
+          <PreviewBlock key={variant} title='Button Group'>
+            {(Object.values(ColorVariantEnum) as ColorVariantEnum[]).map(color => (
+              <Button
+                key={variant + color}
+                variant={variant}
+                color={{color: color}}
+                className="capitalize"
+                size={size}
+                disabled={disabled}
+                active={active}
+              >
+                {/*<BellIcon className='header-icon'/>*/}
+                {color}
+              </Button>
+            ))}
+          </PreviewBlock>
         )
       })}
 
