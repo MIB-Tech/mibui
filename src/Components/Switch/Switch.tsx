b4af86abf5bcc734bbb37b9e40c7ae60d0716f77
Marcio Augusto <marcio_augusto_09@hotmail.com>
2023-10-02 17:24:03 -0300

    feat: switch fixtures

--- src/Components/Switch/Switch.tsx
+++ src/Components/Switch/Switch.tsx
@@ -5,34 +5,35 @@ import { FC, useMemo } from "react";
 const Switch: FC<SwitchProps> = ({ size, color, ...props }) => {
     const classNames = useVariantStyles({color});
     const calcSize = useMemo(() => {
-        const oct_base = 8;
+        const oct_base = 1;
         let sizeType = 1; // small = 0, default = 1, large = 2
         if (size === "sm") sizeType = 0;
         if (size === "lg") sizeType = 2;
         const objSize = {
             switch: {
-                w: ((oct_base * 2) * sizeType) + 44,
-                h: (oct_base * sizeType) + 24
+                w: ((oct_base * 2) * sizeType) + 12,
+                h: (oct_base * sizeType) + 6
             },
             checked_after: {
-                w: (oct_base * sizeType) + 16,
-                h: (oct_base * sizeType) + 16,
-                ml: (oct_base * sizeType) + 20
+                w: (oct_base * sizeType) + 5,
+                h: (oct_base * sizeType) + 5,
+                ml: (4 * sizeType) + 22,
             }
         };
         return objSize;
     }, [size]);
 
     const styles = {
-        root: `mr-2 mt-[0.3rem] h-[${calcSize.switch.h}px] w-[${calcSize.switch.w}px] appearance-none rounded-full p-[4px] ${classNames.background.replace('500', '300')} cursor-pointer outline-none disabled:bg-gray-300 disabled:cursor-not-allowed`,
-        after: `after:absolute after:content-[""] after:h-[${calcSize.checked_after.h}px] after:w-[${calcSize.checked_after.w}px] after:mt-0 after:ml-0 after:bg-white disabled:after:bg-slate-50 after:rounded-full after:border-none after:transition-[background-color_0.25s,transform_0.25s] after:z-[2]`,
-        checked: `checked:after:content-[""] checked:after:absolute checked:${classNames.background} checked:after:bg-white checked:after:h-[${calcSize.checked_after.h}px] checked:after:w-[${calcSize.checked_after.w}px] checked:after:ml-[${calcSize.checked_after.ml}px] checked:after:mt-[0px] checked:after:transition-[background-color_0.25s,transform_0.25s] checked:after:border-none`,
-        focus: ''
+        root: `mr-2 mt-[0.3rem] h-${calcSize.switch.h} w-${calcSize.switch.w} appearance-none rounded-full ${props.disabled ? 'bg-neutral-300' : 'bg-secondary-300'} hover:cursor-pointer disabled:cursor-not-allowed`,
+        before: `before:pointer-events-none before:absolute before:h-3.5 before:w-3.5 before:rounded-full before:bg-transparent before:content-['']`,
+        after: `after:absolute after:z-[2] after:mt-[1.6px] after:ml-[2.6px] after:h-${calcSize.checked_after.h} after:w-${calcSize.checked_after.w} after:rounded-full after:border-none after:bg-neutral-100 after:shadow-[0_0px_3px_0_rgb(0_0_0_/_7%),_0_2px_2px_0_rgb(0_0_0_/_4%)] after:transition-[background-color_0.2s,transform_0.2s] after:content-['']`,
+        checked: `checked:${classNames.background} checked:after:absolute checked:after:z-[2] checked:after:mt-[1.6px] checked:after:ml-[${calcSize.checked_after.ml}px] checked:after:h-${calcSize.checked_after.h} checked:after:w-${calcSize.checked_after.w} checked:after:rounded-full checked:after:border-none checked:after:bg-primary checked:after:shadow-[0_3px_1px_-2px_rgba(0,0,0,0.2),_0_2px_2px_0_rgba(0,0,0,0.14),_0_1px_5px_0_rgba(0,0,0,0.12)] checked:after:transition-[background-color_0.2s,transform_0.2s] checked:after:content-['']`,
+        focus_after: `focus:outline-none focus:ring-0 focus:before:scale-100 focus:before:opacity-[0.12] focus:after:absolute focus:after:z-[1] focus:after:block focus:after:h-${calcSize.checked_after.h} focus:after:w-${calcSize.checked_after.w} focus:after:rounded-full focus:after:content-['']`,
     };
 
     return(
         <input
-            className={`${styles.root} ${styles.after} ${styles.checked}`}
+            className={`${styles.root} ${styles.before} ${styles.after} ${styles.checked} ${styles.focus_after}`}
             type="checkbox"
             role="switch"
             {...props}
