d65fd452738a65808c4e128fbfbb2226075c13a5
Anewaccoun <oneeanew@gmail.com>
2023-10-03 15:16:01 +0100

    Tabs (modified)

--- src/pages/Components/Tabs/Page.tsx
+++ src/pages/Components/Tabs/Page.tsx
@@ -3,28 +3,46 @@ import {Preview, Tab, TabPanel, Tabs, TabsList} from "../../../Components";
 const Page = () => (
   <>
     <Preview title="Default">
-      <Tabs defaultValue={1}>
-        <TabsList>
-          <Tab value={1}>Tab 1</Tab>
-          <Tab value={2}>Tab 2</Tab>
-          <Tab value={3}>Tab 3</Tab>
-        </TabsList>
-        <TabPanel value={1}>Tab Content 1</TabPanel>
-        <TabPanel value={2}>Tab Content 2</TabPanel>
-        <TabPanel value={3}>Tab Content 3</TabPanel>
-      </Tabs>
+      <div className='flex items-center justify-center gap-2'>
+        <Tabs defaultValue={1}>
+          <TabsList>
+            <Tab value={1}>Tab 1</Tab>
+            <Tab value={2}>Tab 2</Tab>
+            <Tab value={3}>Tab 3</Tab>
+          </TabsList>
+          <TabPanel value={1}>Tab Content 1</TabPanel>
+          <TabPanel value={2}>Tab Content 2</TabPanel>
+          <TabPanel value={3}>Tab Content 3</TabPanel>
+        </Tabs>
+      </div>
     </Preview>
     <Preview title="Vertical">
-      <Tabs defaultValue={1} vertical>
-        <TabsList>
-          <Tab value={1}>Tab 1</Tab>
-          <Tab value={2}>Tab 2</Tab>
-          <Tab value={3}>Tab 3</Tab>
-        </TabsList>
-        <TabPanel value={1}>Tab Content 1</TabPanel>
-        <TabPanel value={2}>Tab Content 2</TabPanel>
-        <TabPanel value={3}>Tab Content 3</TabPanel>
-      </Tabs>
+      <div className='flex items-center justify-center gap-2'>
+        <Tabs defaultValue={1} vertical>
+          <TabsList>
+            <Tab value={1}>Tab 1</Tab>
+            <Tab value={2}>Tab 2</Tab>
+            <Tab value={3}>Tab 3</Tab>
+          </TabsList>
+          <TabPanel value={1}>Tab Content 1</TabPanel>
+          <TabPanel value={2}>Tab Content 2</TabPanel>
+          <TabPanel value={3}>Tab Content 3</TabPanel>
+        </Tabs>
+      </div>
+    </Preview>
+    <Preview title="States">
+      <div className='flex items-center justify-center gap-2'>
+        <Tabs defaultValue={1}>
+          <TabsList>
+            <Tab value={1}>Tab 1</Tab>
+            <Tab value={2} disabled>Disabled</Tab>
+            <Tab value={3}>Tab 3</Tab>
+          </TabsList>
+          <TabPanel value={1}>Tab Content 1</TabPanel>
+          <TabPanel value={2}>Tab Content 2</TabPanel>
+          <TabPanel value={3}>Tab Content 3</TabPanel>
+        </Tabs>
+      </div>
     </Preview>
   </>
 )
