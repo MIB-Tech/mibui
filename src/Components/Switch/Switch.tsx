fabd97c31cfed67864b3bd1b85d55803645c4faf
Marcio Augusto <marcio_augusto_09@hotmail.com>
2023-10-04 17:34:00 -0300

    fix: switch base component

--- src/Components/Switch/Switch.tsx
+++ src/Components/Switch/Switch.tsx
@@ -1,41 +1,85 @@
-import {useVariantStyles} from '../../hooks/UseVariantStyles';
 import {SwitchProps} from './Switch.types';
-import {FC, useMemo} from 'react';
+import {FC} from 'react';
+import { Switch as MuiSwitch, SwitchOwnerState, switchClasses } from "@mui/base/Switch";
+import { styled } from "@mui/system";
+import { useVariantStyles } from '../../hooks/UseVariantStyles';
+import { twMerge } from 'tailwind-merge';
 
-const Switch: FC<SwitchProps> = ({ size, color, ...props }) => {
+const Switch: FC<SwitchProps> = ({
+    color = 'primary',
+    ...props
+    }) => {
     const classNames = useVariantStyles({color});
-    const calcSize = useMemo(() => {
-        const oct_base = 1;
-        let sizeType = 1; // small = 0, default = 1, large = 2
-        if (size === "sm") sizeType = 0;
-        if (size === "lg") sizeType = 2;
-        const objSize = {
-            switch: {
-                w: ((oct_base * 2) * sizeType) + 12,
-                h: (oct_base * sizeType) + 6
-            },
-            checked_after: {
-                w: (oct_base * sizeType) + 5,
-                h: (oct_base * sizeType) + 5,
-                ml: (4 * sizeType) + 22,
-            }
-        };
-        return objSize;
-    }, [size]);
 
-    const styles = {
-        root: `mr-2 mt-[0.3rem] h-${calcSize.switch.h} w-${calcSize.switch.w} appearance-none rounded-full ${props.disabled ? 'bg-neutral-300' : 'bg-secondary-300'} hover:cursor-pointer disabled:cursor-not-allowed`,
-        before: `before:pointer-events-none before:absolute before:h-3.5 before:w-3.5 before:rounded-full before:bg-transparent before:content-['']`,
-        after: `after:absolute after:z-[2] after:mt-[1.6px] after:ml-[2.6px] after:h-${calcSize.checked_after.h} after:w-${calcSize.checked_after.w} after:rounded-full after:border-none after:bg-neutral-100 after:shadow-[0_0px_3px_0_rgb(0_0_0_/_7%),_0_2px_2px_0_rgb(0_0_0_/_4%)] after:transition-[background-color_0.2s,transform_0.2s] after:content-['']`,
-        checked: `checked:${classNames.background} checked:after:absolute checked:after:z-[2] checked:after:mt-[1.6px] checked:after:ml-[${calcSize.checked_after.ml}px] checked:after:h-${calcSize.checked_after.h} checked:after:w-${calcSize.checked_after.w} checked:after:rounded-full checked:after:border-none checked:after:bg-primary checked:after:shadow-[0_3px_1px_-2px_rgba(0,0,0,0.2),_0_2px_2px_0_rgba(0,0,0,0.14),_0_1px_5px_0_rgba(0,0,0,0.12)] checked:after:transition-[background-color_0.2s,transform_0.2s] checked:after:content-['']`,
-        focus_after: `focus:outline-none focus:ring-0 focus:before:scale-100 focus:before:opacity-[0.12] focus:after:absolute focus:after:z-[1] focus:after:block focus:after:h-${calcSize.checked_after.h} focus:after:w-${calcSize.checked_after.w} focus:after:rounded-full focus:after:content-['']`,
+    const slotProps = {
+        track: (ownerState: SwitchOwnerState) => ({
+            className: twMerge(
+                ownerState.checked ? classNames.background : classNames.background.replace('500', '300')
+            )
+        }),
     };
 
+    const Root = styled('span')(() => `
+        font-size: 0;
+        position: relative;
+        display: inline-block;
+        width: 40px;
+        height: 24px;
+        margin: 10px;
+        cursor: pointer;
+        &.${switchClasses.disabled} {
+            opacity: 0.4;
+            cursor: not-allowed;
+        }
+        & .${switchClasses.track} {
+            border-radius: 16px;
+            display: block;
+            height: 100%;
+            width: 100%;
+            position: absolute;
+        }
+        & .${switchClasses.thumb} {
+            display: block;
+            width: 16px;
+            height: 16px;
+            top: 4px;
+            left: 4px;
+            border-radius: 16px;
+            background-color: #fff;
+            position: relative;
+            transition-property: all;
+            transition-timing-function: cubic-bezier(0.4, 0, 0.2, 1);
+            transition-duration: 120ms;
+        }
+        &.${switchClasses.focusVisible} .${switchClasses.thumb} {
+            box-shadow: 0 0 1px 8px rgba(0, 0, 0, 0.25);
+        }
+        &.${switchClasses.checked} {
+            .${switchClasses.thumb} {
+                left: 20px;
+                top: 4px;
+                background-color: #fff;
+            }
+        }
+        & .${switchClasses.input} {
+            cursor: inherit;
+            position: absolute;
+            width: 100%;
+            height: 100%;
+            top: 0;
+            left: 0;
+            opacity: 0;
+            z-index: 1;
+            margin: 0;
+        }
+        `
+    );
+
     return(
-        <input
-            className={`${styles.root} ${styles.before} ${styles.after} ${styles.checked} ${styles.focus_after}`}
-            type="checkbox"
-            role="switch"
+        <MuiSwitch
+            id="defaultSwitch"
+            slots={{ root: Root }}
+            slotProps={slotProps}
             {...props}
         />
     );
--- src/pages/Forms/Switch/SwitchPage.tsx
+++ src/pages/Forms/Switch/SwitchPage.tsx
@@ -1,39 +1,18 @@
-import { COLORS } from "../../../@types/Color";
+// import { COLORS } from "../../../@types/Color";
 import { Preview } from "../../../Components";
-import { Switch } from '../../../Components/Switch';
-// import { useState } from "react";
+import { Switch } from "../../../Components/Switch";
 
 const Page = () => {
-    // const [check, setCheck] = useState({});
-
-    // const checkSwitch = (event: React.ChangeEvent<HTMLInputElement>) => {
-    //     // console.log(event);
-    //     setCheck({
-    //         ...check,
-    //         [event.target.name]: event.target.checked
-    //     });
-    // }
-
     return(
         <div className="flex flex-col gap-12">
             <Preview title="Default" className="flex justify-center">
                 <Switch />
             </Preview>
             <Preview title="Disabled State" className="flex justify-center">
-                <Switch disabled />
             </Preview>
             <Preview title="Sizing" className="grid grid-cols-3 items-center justify-items-center">
-                <Switch size="sm" />
-                <Switch size="md" />
-                <Switch size="lg" />
             </Preview>
             <Preview title="Colors" className="grid grid-cols-3 gap-6 justify-items-center">
-                {COLORS.map((color, index) => (
-                    <Switch
-                        key={index}
-                        color={color}
-                    />
-                ))}
             </Preview>
         </div>
     )
