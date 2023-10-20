9d94bf3958af84be265524cc2c61a0bd80ba48d3
Marcio Augusto <marcio_augusto_09@hotmail.com>
2023-10-20 11:04:39 -0300

    feat: radio and switch responsivity check

--- src/pages/Forms/Radio/RadioPage.tsx
+++ src/pages/Forms/Radio/RadioPage.tsx
@@ -5,11 +5,11 @@ import Radio from "../../../Components/Radio/Radio";
 const Page = () => {
     return(
         <div className="flex flex-col gap-12">
-            <Preview title="Default" className="flex items-center justify-center gap-10">
+            <Preview title="Default" className="flex items-center justify-center gap-10 px-0">
                 <Radio />
                 <Radio defaultChecked />
             </Preview>
-            <Preview title="Colors" className="grid grid-cols-3 gap-6 justify-items-center">
+            <Preview title="Colors" className="grid grid-cols-3 gap-6 justify-items-center px-0">
                 {COLORS.map((color, index) =>
                 <Radio 
                     key={index} 
@@ -17,12 +17,12 @@ const Page = () => {
                     defaultChecked
                 />)}
             </Preview>
-            <Preview title="Sizing" className="flex items-center justify-center gap-10">
+            <Preview title="Sizing" className="flex items-center justify-center gap-10 px-0">
                 <Radio defaultChecked />
                 <Radio size="md" defaultChecked />
                 <Radio size="lg" defaultChecked />
             </Preview>
-            <Preview title="States" className="flex items-center justify-center gap-10">
+            <Preview title="States" className="flex items-center justify-center gap-10 px-0">
                 <Radio disabled />
                 <Radio disabled defaultChecked />
             </Preview>
--- src/pages/Forms/Switch/SwitchPage.tsx
+++ src/pages/Forms/Switch/SwitchPage.tsx
@@ -5,20 +5,20 @@ import { Switch } from "../../../Components/Switch";
 const Page = () => {
     return(
         <div className="flex flex-col gap-12">
-            <Preview title="Default" className="flex justify-center">
+            <Preview title="Default" className="flex justify-center px-0">
                 <Switch />
                 <Switch defaultChecked />
             </Preview>
-            <Preview title="Disabled State" className="flex justify-center">
+            <Preview title="Disabled State" className="flex justify-center px-0">
                 <Switch disabled />
                 <Switch disabled defaultChecked />
             </Preview>
-            <Preview title="Sizing" className="grid grid-cols-3 items-center justify-items-center">
+            <Preview title="Sizing" className="grid grid-cols-3 items-center justify-items-center px-0">
                 <Switch size="sm" />
                 <Switch size="md" />
                 <Switch size="lg" />
             </Preview>
-            <Preview title="Colors" className="grid grid-cols-3 gap-6 justify-items-center">
+            <Preview title="Colors" className="grid grid-cols-3 gap-6 justify-items-center px-0">
                 {COLORS.map((item, index) => <Switch key={index} color={item} defaultChecked />)}
             </Preview>
         </div>
