375bab3fbb0bd4582106b1b4cd7f6851b6b33b5e
ABDERRAHMANE ELFANNIR <a.elfannir@akdital.ma>
2023-11-30 09:57:32 +0100

    UK-252

--- src/pages/Components/List/Page.tsx
+++ src/pages/Components/List/Page.tsx
@@ -1,4 +1,5 @@
-import {List, ListItem, Preview} from '../../../Components';
+import {Icon, List, ListItem, Preview} from '../../../Components';
+import {HomeIcon, ShoppingBagIcon} from '@heroicons/react/20/solid';
 
 const Page = () => (
   <div className='flex flex-col gap-12'>
@@ -7,6 +8,28 @@ const Page = () => (
         <ListItem>Item 1</ListItem>
         <ListItem>Item 2</ListItem>
         <ListItem>Item 3</ListItem>
+      </List>
+    </Preview>
+    <Preview title='Custom Item' className='flex justify-center'>
+      <List className='bg-white rounded'>
+        <ListItem className='flex gap-3'>
+          <Icon icon={HomeIcon} size={5}/>
+          <div>
+            Dashboard
+          </div>
+        </ListItem>
+        <ListItem className='flex gap-3'>
+          <Icon icon={ShoppingBagIcon} size={5}/>
+          <div>
+            E-commerce
+          </div>
+        </ListItem>
+        <ListItem className='flex gap-3' active>
+          <Icon icon={ShoppingBagIcon} size={5}/>
+          <div>
+            E-commerce
+          </div>
+        </ListItem>
       </List>
     </Preview>
   </div>
