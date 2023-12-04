b10b6a1edf38472f8d5bacd2fa73ac479547c85f
ABDERRAHMANE ELFANNIR <a.elfannir@akdital.ma>
2023-12-04 12:24:02 +0100

    Sidebar

--- src/Components/Sidebar/Sidebar.types.ts
+++ src/Components/Sidebar/Sidebar.types.ts
@@ -1,12 +1,11 @@
 import {ListItemProps, ListProps} from '../List/List.types.ts';
 import {SvgIconProps} from '../SvgIcon/SvgIcon.types.ts';
-import {HTMLAttributes} from 'react';
+import {ReactNode} from 'react';
 
 export type SidebarProps = ListProps
 
 export type SidebarItemProps = {
-  hasChildren?: boolean
 } & ListItemProps & Partial<Pick<SvgIconProps, 'icon'>>
 export type SidebarAccordionProps = {
-  label: string
-} & HTMLAttributes<HTMLDivElement>
+  label: ReactNode
+} & SidebarItemProps
